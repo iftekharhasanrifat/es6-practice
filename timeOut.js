9606dbb2ee0d4d6fc1a79a333d69fb5875273ec8
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-02-07 12:14:05 +0000

    timeout

---
+++ timeOut.js
@@ -0,0 +1,14 @@
+// settimeout executes for one time
+// setinterval executes for infinite time after the the interval 
+
+console.log(2222);
+// setTimeout(()=>{
+//     console.log('See ya');
+// },1000);
+
+setInterval(()=>{
+    console.log("doing it");
+},3000)
+console.log(4444);
+console.log(4444);
+console.log(4444);
